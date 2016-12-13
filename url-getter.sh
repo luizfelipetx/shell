9378c77e2520937d80ce0e11a9e8f95e1c12a2b5
felipe <luizfelipetx@gmail.com>
2016-12-12 22:24:15 -0200

    adding routine to send request to url

---
+++ url-getter.sh
@@ -0,0 +1,19 @@
+#
+#get url from urls list , save the status response , and last date called, can be used in crontab
+#
+# use one line == one url
+# example urls.txt above:
+#
+#http://google.com
+#http://yahoo.com
+#http://ig.com.br
+#
+
+addlog(){
+        echo "getdata ? $1" >> logrequest.log
+}
+
+for url in $(cat /opt/cron/urls.txt);do
+        wget --spider -S "${url}" 2>&1 | grep "HTTP/" | awk -v date="$(date +"%Y-%m-%d %r")"   '{print $2 ,"---", date }' >>/opt/cron/logrequest.log
+done
+echo "last run $(date)" >> /opt/cron/logcron.log
